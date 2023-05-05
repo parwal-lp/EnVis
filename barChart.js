2eebfebb3a358d7f45d1cd648719443be2504c8e
parwal <parwal.lp@gmail.com>
2023-05-05 20:13:21 +0200

    importato bootstrap e creato header

--- barChart.js
+++ barChart.js
@@ -3,7 +3,7 @@ var colorArray = ['#d73027','#fc8d59','#fee08b','#d9ef8b','#91cf60','#1a9850']; 
 var levelArray = ['extremely poor', 'very poor', 'poor', 'moderate', 'fair', 'good'];
 
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 40, left: 90},
+var margin = {top: 20, right: 30, bottom: 40, left: 100},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
